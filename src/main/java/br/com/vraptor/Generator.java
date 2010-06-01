21e0f6c0a68cd2b5e9d34cac380bd3d412f3d753
rodolfoliviero <rodolfoliviero@gmail.com>
2010-06-01 14:06:51 -0300

    refactoring

--- src/main/java/br/com/vraptor/Generator.java
+++ src/main/java/br/com/vraptor/Generator.java
@@ -7,27 +7,28 @@ public class Generator {
 	
 	public static void main(String[] args) throws Exception {
 		if (args.length == 1) {
-			String project = args[0];
-			Scaffold scaffold = new Scaffold(project);
-			scaffold.createProject();
-			scaffold.createPom();
-			scaffold.createMainJava();
-			scaffold.createMainResources();
-			scaffold.createWebApp();
-			scaffold.createTestJava();
-			scaffold.createTestResources();
+			String projectName = args[0];
+			
+			BlankProject project = new BlankProject(projectName);
+			project.createProject();
+			project.createPom();
+			project.createMainJava();
+			project.createMainResources();
+			project.createWebApp();
+			project.createTestJava();
+			project.createTestResources();
 		}
 
 		if (args.length > 1) {
 			String model = args[1];
-			Scaffold scaffold = new Scaffold(model);
 			
 			List<AttributeWrapper> attributes = new ArrayList<AttributeWrapper>();
 			for(int i = 2; i < args.length; i++) {
 				String[] attribute = args[i].split(":");
 				attributes.add(new AttributeWrapper(attribute[0], attribute[1]));
 			}
-			scaffold.setAttributes(attributes);
+			
+			Scaffold scaffold = new Scaffold(model, attributes);
 			scaffold.generateModel();
 			scaffold.generateController();
 			scaffold.generateViews();
--- src/main/java/br/com/vraptor/Scaffold.java
+++ src/main/java/br/com/vraptor/Scaffold.java
@@ -1,8 +1,5 @@
 package br.com.vraptor;
 
-import static br.com.vraptor.FileUtils.buildDirectoryName;
-import static br.com.vraptor.FileUtils.copy;
-import static br.com.vraptor.FileUtils.create;
 import static br.com.vraptor.FileUtils.writeFile;
 
 import java.io.File;
@@ -19,76 +16,12 @@ import freemarker.template.utility.StringUtil;
 
 public class Scaffold {
 	
-	private String project;
 	private String model;
 	private List<AttributeWrapper> attributes;
 	
-	public Scaffold(String project) {
-		this.project = project;
-		//temp im gonna sleep right now
-		this.model = project.toLowerCase();
-	}
-	
-	public void createProject() throws IOException {
-		create(project);
-	}
-	
-	public void createMainJava() throws IOException {
-		String mainJava = project + buildDirectoryName("src", "main", "java");
-		String appPath = mainJava + buildDirectoryName("app");
-		String infrastructure = appPath + buildDirectoryName("infrastructure");
-		create(mainJava);
-		create(appPath);
-		create(appPath + buildDirectoryName("models"));
-		create(appPath + buildDirectoryName("controllers"));
-		create(infrastructure);
-		copy("/scaffold/FreemarkerPathResolver.java", infrastructure + buildDirectoryName("FreemarkerPathResolver.java"));
-	}
-	
-	public void createMainResources() throws IOException {
-		String mainResources = project + buildDirectoryName("src", "main", "resources");
-		String metaInf = mainResources + buildDirectoryName("META-INF");
-		create(mainResources);
-		copy("/scaffold/log4j.xml", mainResources + buildDirectoryName("log4j.xml"));
-		create(metaInf);
-		copy("/scaffold/META-INF/persistence.xml", metaInf + buildDirectoryName("persistence.xml"));
-	}
-	
-	public void createWebApp() throws IOException {
-		String webapp = project + buildDirectoryName("src", "main", "webapp");
-		String webInf = webapp + buildDirectoryName("WEB-INF");
-		String decorators = webapp + buildDirectoryName("decorators");
-		
-		create(webapp);
-		copy("/scaffold/index.jsp", webapp + buildDirectoryName("index.jsp"));
-		
-		create(webInf);
-		copy("/scaffold/WEB-INF/decorators.xml", webInf + buildDirectoryName("decorators.xml"));
-		copy("/scaffold/WEB-INF/web.xml",  webInf + buildDirectoryName("web.xml"));
-		create(webInf + buildDirectoryName("freemarker"));
-		
-		create(decorators);
-		copy("/scaffold/decorators/main.ftl", decorators + buildDirectoryName("main.ftl"));
-	}
-	
-	public void createTestJava() {
-		create(project + buildDirectoryName("src", "test", "java"));
-	}
-	
-	public void createTestResources() {
-		create(project + buildDirectoryName("src", "test", "resources"));
-	}
-	
-	public void createPom() throws IOException, TemplateException {
-		String pom = "pom.xml";
-		Template template = TemplateHandler.getInstance().loadTemplate(pom);
-		
-		Map<String, String> content = new HashMap<String, String>();
-		content.put("GROUP_ID", project);
-		content.put("ARTIFACT_ID", project);
-		content.put("NAME", project);
-
-		writeFile(template, content, project + buildDirectoryName(pom));
+	public Scaffold(String model, List<AttributeWrapper> attributes) {
+		this.model = model.toLowerCase();
+		this.attributes = attributes;
 	}
 	
 	public void generateModel() throws IOException, TemplateException {
@@ -123,9 +56,5 @@ public class Scaffold {
 		String filename = "src/main/webapp/WEB-INF/freemarker/" + model.toLowerCase();
 		new File(filename).mkdirs();
 		writeFile(templateModel, content, filename  + "/index.ftl");
-	}
-
-	public void setAttributes(List<AttributeWrapper> attributes) {
-		this.attributes = attributes;
 	}
 }
