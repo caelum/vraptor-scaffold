7d935d10b7b715fc715d5e945321148495302880
rodolfoliviero <rodolfoliviero@gmail.com>
2010-08-31 14:26:48 -0300

    jsp generator spec

--- lib/generators/scaffold_generator/scaffold_generator.rb
+++ lib/generators/scaffold_generator/scaffold_generator.rb
@@ -12,8 +12,8 @@ class ScaffoldGenerator
     ModelGenerator.new(model, attributes).build
     RepositoryGenerator.new(model, attributes).build
     ControllerGenerator.new(model, attributes).build
-    #FreemarkerGenerator.new(model, attributes).build
-    JspGenerator.new(model, attributes).build
+    FreemarkerGenerator.new(model, attributes).build
+    #JspGenerator.new(model, attributes).build
   end
 
   private 
--- spec/lib/generators/scaffold_generator/jsp_generator/jsp_generator_spec.rb
+++ spec/lib/generators/scaffold_generator/jsp_generator/jsp_generator_spec.rb
@@ -1,5 +1,33 @@
 require File.expand_path(File.dirname(__FILE__) + "/../../../../spec_helper")
 
 describe JspGenerator do
+  before(:each) do
+    @views_path = "#{Configuration::WEB_INF}/jsp"
+    @generator = JspGenerator.new("product", build_attributes)
+    @generator.build
+  end
 
+  after(:each) do
+    FileUtils.remove_dir("src") 
+  end
+
+  it "should create index view" do
+    File.exist?("#{@views_path}/product/index.jsp").should be_true 
+  end
+
+  it "should create show view" do
+    File.exist?("#{@views_path}/product/show.jsp").should be_true 
+  end
+
+  it "should create form view" do
+    File.exist?("#{@views_path}/product/form.jsp").should be_true 
+  end
+
+  it "should create new view" do
+    File.exist?("#{@views_path}/product/newProduct.jsp").should be_true 
+  end
+
+  it "should create edit view" do
+    File.exist?("#{@views_path}/product/edit.jsp").should be_true 
+  end
 end	
