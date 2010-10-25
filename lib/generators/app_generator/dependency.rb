882983034416f7f68771a137ef4dbcfe3a2e260c
Luiz Fernando Signorelli <luiz.sg@gmail.com>
2010-10-25 00:47:51 -0200

    refactoring dependency.rb and pom.erb to make ivy and gradle support easyer

--- lib/generators/app_generator/dependency.rb
+++ lib/generators/app_generator/dependency.rb
@@ -1,26 +1,38 @@
 class Dependency
 
-  attr_accessor :group, :name, :version
+  attr_accessor :group, :name, :version, :exclusions
 
-  def initialize group, name, version
+  def initialize group, name, version=nil, exclusions=[]
     @group = group
     @name = name
     @version = version
+    @exclusions = exclusions
   end
 
-  def self.compile_scope
-    [self.new("br.com.caelum", "vraptor", "3.2.0"), self.new("org.hsqldb", "hsqldb", "2.0.0"),
-      self.new("opensymphony", "sitemesh", "2.4.2"), self.new("javax.servlet", "jstl", "1.2"),
-      self.new("javax.persistence", "persistence-api", "1.0"), 
-      self.new("org.hibernate", "hibernate-entitymanager", "3.4.0.GA"),
-      self.new("org.hibernate", "hibernate-core", "3.3.2.GA"),
-      self.new("org.hibernate", "hibernate-validator", "3.1.0.GA"),
-      self.new("joda-time", "joda-time", "1.6")]
+  def self.compile_scope(spring_version="2.5")
+    dependencies = []
+    if spring_version == "3.0"
+      dependencies << self.new("br.com.caelum", "vraptor", "3.2.0",[self.new("org.springframework", "spring")])
+      dependencies = dependencies + self.compile_scope_default_libs + self.compile_scope_spring3_libs
+    else
+      dependencies << self.new("br.com.caelum", "vraptor", "3.2.0") 
+      dependencies = dependencies + self.compile_scope_default_libs
+    end
+    dependencies
   end
 
-  def self.spring_3_compile_scope
+  def self.compile_scope_default_libs
+    [ self.new("org.hsqldb", "hsqldb", "2.0.0"),
+        self.new("opensymphony", "sitemesh", "2.4.2"), self.new("javax.servlet", "jstl", "1.2"),
+        self.new("javax.persistence", "persistence-api", "1.0"), 
+        self.new("org.hibernate", "hibernate-entitymanager", "3.4.0.GA"),
+        self.new("org.hibernate", "hibernate-core", "3.3.2.GA"),
+        self.new("org.hibernate", "hibernate-validator", "3.1.0.GA"),
+        self.new("joda-time", "joda-time", "1.6")]
+  end
+
+  def self.compile_scope_spring3_libs
     [self.new("org.springframework", "spring-web", "3.0.4.RELEASE"), self.new("com.thoughtworks.xstream", "xstream", "1.3.1")]
-
   end
 
   def self.test_scope
@@ -31,5 +43,9 @@ class Dependency
 
   def self.provided_scope
     [self.new("javax.servlet", "servlet-api", "2.5")]
+  end
+
+  def has_exclusions?
+    !@exclusions.empty?
   end
 end
