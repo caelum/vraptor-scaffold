7ea8c9f3c6422eaa2595a0a403c7f884dc5de451
rodolfoliviero <rodolfoliviero@gmail.com>
2011-02-20 10:07:55 -0300

    better controller template

--- spec/lib/generators/scaffold_generator/controller_generator/templates/ProductController.java
+++ spec/lib/generators/scaffold_generator/controller_generator/templates/ProductController.java
@@ -6,7 +6,6 @@ import app.models.Product;
 import app.repositories.ProductRepository;
 import br.com.caelum.vraptor.Delete;
 import br.com.caelum.vraptor.Get;
-import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Put;
 import br.com.caelum.vraptor.Resource;
@@ -26,14 +25,12 @@ public class ProductController {
 		this.validator = validator;
 	}
 	
-	@Get
-	@Path("/products")
+	@Get("/products")
 	public List<Product> index() {
 		return repository.findAll();
 	}
 	
-	@Post
-	@Path("/products")
+	@Post("/products")
 	public void create(Product product) {
 		validator.validate(product);
 		validator.onErrorUsePageOf(this).newProduct();
@@ -41,14 +38,12 @@ public class ProductController {
 		result.redirectTo(this).index();
 	}
 	
-	@Get
-	@Path("/products/new")
+	@Get("/products/new")
 	public Product newProduct() {
 		return new Product();
 	}
 	
-	@Put
-	@Path("/products")
+	@Put("/products")
 	public void update(Product product) {
 		validator.validate(product);
 		validator.onErrorUsePageOf(this).edit(product);
@@ -56,23 +51,19 @@ public class ProductController {
 		result.redirectTo(this).index();
 	}
 	
-	@Get
-	@Path("/products/{product.id}/edit")
+	@Get("/products/{product.id}/edit")
 	public Product edit(Product product) {
 		return repository.find(product.getId());
 	}
 
-	@Get
-	@Path("/products/{product.id}")
+	@Get("/products/{product.id}")
 	public Product show(Product product) {
 		return repository.find(product.getId());
 	}
 
-	@Delete
-	@Path("/products/{product.id}")
+	@Delete("/products/{product.id}")
 	public void destroy(Product product) {
 		repository.destroy(repository.find(product.getId()));
 		result.redirectTo(this).index();  
 	}
-	
 }
