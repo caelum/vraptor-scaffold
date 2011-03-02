2e25626dbeac3b74dbb8f3ce1d7d981c63dea7fa
rodolfoliviero <rodolfoliviero@gmail.com>
2011-03-02 14:44:47 -0300

    fix identation

--- lib/vraptor-scaffold/configuration.rb
+++ lib/vraptor-scaffold/configuration.rb
@@ -1,11 +1,11 @@
 class Configuration
-  MAIN_SRC="src/main/java"
-  MAIN_RESOURCES="src/main/resources"
-  TEST_SRC="src/test/java"
-  TEST_RESOURCES="src/test/resources"
-  WEB_APP="src/main/webapp"
-  WEB_INF="#{WEB_APP}/WEB-INF"
-  META_INF="#{WEB_APP}/META-INF"
+  MAIN_SRC = "src/main/java"
+  MAIN_RESOURCES = "src/main/resources"
+  TEST_SRC = "src/test/java"
+  TEST_RESOURCES = "src/test/resources"
+  WEB_APP = "src/main/webapp"
+  WEB_INF = "#{WEB_APP}/WEB-INF"
+  META_INF = "#{WEB_APP}/META-INF"
   FILENAME = "#{META_INF}/vraptor-scaffold.properties"
 
   def self.config
@@ -22,10 +22,10 @@ class Configuration
 
   def self.main_class_path *path
     File.join MAIN_SRC, package.gsub(".", File::Separator), path
-  end  
+  end
 
   def self.test_class_path *path
     File.join TEST_SRC, package.gsub(".", File::Separator), path
-  end 
+  end
 
 end
