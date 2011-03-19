06c32538c5f0777e9bd63f5b9e7a04183e309788
rodolfoliviero <rodolfoliviero@gmail.com>
2011-03-19 16:48:34 -0300

    sample implementation of login

---
+++ sample/src/main/java/app/repositories/Repository.java
@@ -0,0 +1,48 @@
+package app.repositories;
+
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+public abstract class Repository<T, I extends Serializable> {
+	
+	protected final EntityManager entityManager;
+	protected final Class<T> clazz;
+
+	protected Repository(EntityManager entityManager) {
+		this.entityManager = entityManager;
+		
+		@SuppressWarnings("unchecked")
+		Class<T> clazz = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+
+		this.clazz = clazz;
+	}
+	
+	public void create(T entity) {
+		entityManager.persist(entity);
+	}
+	
+	public void update(T entity) {
+		entityManager.merge(entity);
+	}
+	
+	public void destroy(T entity) {
+		entityManager.remove(entity);
+	}
+	
+	public T find(I id) {
+		return entityManager.find(clazz, id);
+	}
+	
+	public List<T> findAll() {
+		Query query = entityManager.createQuery("from " + clazz.getName());
+
+		@SuppressWarnings("unchecked")
+		List<T> resultList = query.getResultList();
+
+		return resultList;
+	}
+}
