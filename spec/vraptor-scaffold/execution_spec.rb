2aa65acf03719ebf40b4f8637ccb95a7c403b9c0
rodolfoliviero <rodolfoliviero@gmail.com>
2011-04-13 00:07:44 -0300

    execution spec

---
+++ spec/vraptor-scaffold/execution_spec.rb
@@ -0,0 +1,44 @@
+require "spec_helper"
+
+describe VraptorScaffold::Execution do
+
+  before(:each) do
+    @execution = VraptorScaffold::Execution.new
+  end
+
+  context "discover runner for action" do
+    it "should be app generator when no action action" do
+      @execution.runner_for(nil).should == VraptorScaffold::Runner::Generator
+    end
+
+    it "should be app generator when -h action" do
+      @execution.runner_for("-h").should == VraptorScaffold::Runner::Generator
+    end
+
+    it "should be app generator when --help action" do
+      @execution.runner_for("--help").should == VraptorScaffold::Runner::Generator
+    end
+
+    it "should be app generator when new action" do
+      @execution.runner_for("new").should == VraptorScaffold::Runner::Generator
+    end
+
+    it "should be app generator when scaffold action" do
+      @execution.runner_for("scaffold").should == VraptorScaffold::Runner::Scaffold
+    end
+
+    it "should be commands when scaffold when unknown action" do
+      @execution.runner_for("xpto").should == VraptorScaffold::Runner::CommandsHelp
+    end
+  end
+
+  context "run action" do
+    it "should discover runner and run" do
+      runner = mock(VraptorScaffold::Runner::Generator)
+      runner.stub!(:new).and_return runner
+      runner.should_receive(:run).with(['app'])
+      @execution.stub!(:runner_for).with('new').and_return(runner)
+      @execution.run(['new', 'app'])
+    end
+  end
+end
