8d879b99be84f847a448771fe049580bdbbf65dd
rodolfoliviero <rodolfoliviero@gmail.com>
2011-04-14 00:35:12 -0300

    plugin spec

---
+++ lib/vraptor-scaffold/generators/plugin.rb
@@ -0,0 +1,23 @@
+ def run(args)
+        if args[0]=="install" && args.size>1
+          uri = args[1]
+          if uri[0..3]!="http"
+            # soon... soon
+            help
+          else
+            response = Restfulie.at(uri).get
+            # TODO: put #response into a file called #{uri}.jar
+            # should be done through ruby
+            # save this jar file into WEB-INF/lib
+          end
+        else
+          help
+        end
+      end
+
+      def help
+        puts "To install a plugin use:"
+        puts
+        # puts "vraptor plugin install plugin_name"
+        puts "vraptor plugin install http://www.yourserver.com/plugin.jar"
+      end
---
+++ spec/vraptor-scaffold/runner/plugin_spec.rb
@@ -0,0 +1,5 @@
+require "spec_helper"
+
+describe VraptorScaffold::Runner::Plugin do
+
+end
