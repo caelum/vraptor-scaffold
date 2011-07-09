d02bedcb3e3ea8f99e83652962c4dc275fa35237
Rodolfo Liviero <rodolfoliviero@gmail.com>
2011-07-09 12:35:34 -0300

    returning default values to maintain backward compatibility. Closes #51

--- spec/vraptor-scaffold/configuration_spec.rb
+++ spec/vraptor-scaffold/configuration_spec.rb
@@ -30,14 +30,14 @@ describe Configuration do
     Configuration::META_INF.should == "#{Configuration::WEB_APP}/META-INF"
   end
 
-  it "should configure properties name" do
+  it "should configure scaffold properties" do
     Configuration::FILENAME.should == "#{Configuration::META_INF}/vraptor-scaffold.properties"
   end
 
   context "load config file" do
     before(:each) do
       config = {"template_engine" => "jsp", "package" => "br.com.caelum", "orm" => "jpa", "models_package" => "model",
-      "controllers_package" => "controller", "repositories_package" => "repository"}
+                "controllers_package" => "controller", "repositories_package" => "repository"}
       YAML.stub!(:load_file).with(Configuration::FILENAME).and_return(config)
     end
 
@@ -80,19 +80,64 @@ describe Configuration do
     it "should build test class path with base package" do
       Configuration.test_class_path("models", "Product.java").should == "src/test/java/br/com/caelum/models/Product.java"
     end
+  end
+  context "hibernate" do
+    before(:each) do
+      config = {"template_engine" => "jsp", "package" => "br.com.caelum", "orm" => "hibernate"}
+      YAML.stub!(:load_file).with(Configuration::FILENAME).and_return(config)
+    end
+    it "should know orm" do
+      Configuration.orm.should == "hibernate"
+    end
 
-    context "hibernate" do
-      before(:each) do
-        config = {"template_engine" => "jsp", "package" => "br.com.caelum", "orm" => "hibernate"}
-        YAML.stub!(:load_file).with(Configuration::FILENAME).and_return(config)
-      end
-      it "should know orm" do
-        Configuration.orm.should == "hibernate"
-      end
+    it "orm should be hibernate" do
+      Configuration.hibernate?.should be_true
+    end
+  end
+  context "properties without default configs" do
+    before(:each) do
+      config = {}
+      YAML.stub!(:load_file).with(Configuration::FILENAME).and_return(config)
+    end
 
-      it "orm cannot be hibernate" do
-        Configuration.hibernate?.should be_true
-      end
+    it "should know models package" do
+      Configuration.models_package.should == "models"
+    end
+
+    it "should know controllers package" do
+      Configuration.controllers_package.should == "controllers"
+    end
+
+    it "should know repositories package" do
+      Configuration.repositories_package.should == "repositories"
+    end
+
+    it "should know full models package" do
+      Configuration.full_models_package.should == "app.models"
+    end
+
+    it "should know template engine" do
+      Configuration.template_engine.should == "jsp"
+    end
+
+    it "should know base package" do
+      Configuration.package.should == "app"
+    end
+
+    it "should know orm" do
+      Configuration.orm.should == "jpa"
+    end
+
+    it "orm cannot be hibernate" do
+      Configuration.hibernate?.should be_false
+    end
+
+    it "should build main class path with base package" do
+      Configuration.main_class_path("models", "Product.java").should == "src/main/java/app/models/Product.java"
+    end
+
+    it "should build test class path with base package" do
+      Configuration.test_class_path("models", "Product.java").should == "src/test/java/app/models/Product.java"
     end
   end
 end
