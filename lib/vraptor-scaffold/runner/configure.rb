c1cfb48ee49e38db78ef465009273de0d1d395c4
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2011-07-25 21:35:00 -0300

    help support for this guy

--- lib/vraptor-scaffold/runner/configure.rb
+++ lib/vraptor-scaffold/runner/configure.rb
@@ -4,11 +4,17 @@ module VraptorScaffold
     class Configure
 
       def run(args)
-        plugin_name = args.first
-        require plugin_name
-        plugin_name.gsub('-','_').camelize.constantize.new(args).invoke_all
+        if VraptorScaffold::Runner::Help.help?(args.first) || args.size < 2
+          puts "vraptor configure usage:"
+          puts "\tvraptor configure PLUGIN_NAME"
+        elsif File.exist?("src")
+          plugin_name = args.first
+          require plugin_name
+          plugin_name.gsub('-','_').camelize.constantize.new(args).invoke_all
+        else
+          puts "To run vraptor plugin please go to the project root folder."
+        end
       end
     end
   end
 end
-
