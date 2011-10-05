94c5c3791d9c8adff93d21a219c8f1637e692d9a
Arthur Moura Carvalho <armoucar@gmail.com>
2011-10-05 10:43:09 -0300

    objectify dependency for gae projects

--- spec/vraptor-scaffold/generators/app/dependency_manager_spec.rb
+++ spec/vraptor-scaffold/generators/app/dependency_manager_spec.rb
@@ -1,19 +1,28 @@
 require File.expand_path(File.dirname(__FILE__) + "/../../../spec_helper")
 
 describe DependencyManager do
-  
+
   before(:each) do
     others_dependency = mock(Dependency)
     others_dependency.stub!(:exclusions).and_return([])
     Dependency.stub!(:new).and_return(others_dependency)
     @dependency = mock(Dependency)
   end
-  
+
   context "compile scope" do
     it "should include freemarker dependency when template is freemarker" do
       options = {:template_engine => "ftl"}
       Dependency.stub!(:new).with("org.freemarker", "freemarker", "2.3.18").and_return(@dependency)
       DependencyManager.new(options).compile_scope.include?(@dependency).should be_true
     end
+
+    context "dependencies for a gae app" do
+      it "should include gae dependencies when gae project options is selected" do
+        options = {:gae => true}
+        Dependency.stub!(:new).with("com.googlecode.objectify", "objectify", "2.2.3").and_return(@dependency)
+        DependencyManager.new(options).compile_scope.include?(@dependency).should be_true
+      end
+    end
   end
+
 end
