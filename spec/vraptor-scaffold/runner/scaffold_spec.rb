da17aba863bc8aee8ee8e171046a45bfd19fbecd
Arthur Moura Carvalho <armoucar@gmail.com>
2011-10-24 20:49:55 -0200

    renamed instance variable in search of cohesion

--- spec/vraptor-scaffold/runner/scaffold_spec.rb
+++ spec/vraptor-scaffold/runner/scaffold_spec.rb
@@ -4,13 +4,13 @@ describe VraptorScaffold::Runner::Scaffold do
 
   before(:all) do
     @help = VraptorScaffold::Runner::Help
-    @scaffold_runner = VraptorScaffold::Runner::Scaffold
+    @runner_scaffold = VraptorScaffold::Runner::Scaffold
   end
 
   it "should print help command" do
     @help.stub!(:help).with("--help").and_return true
     ScaffoldGenerator.should_receive(:start).with(["-h"])
-    @scaffold_runner.new.run(["--help"])
+    @runner_scaffold.new.run(["--help"])
   end
 
   context "scaffold generator" do
@@ -23,13 +23,13 @@ describe VraptorScaffold::Runner::Scaffold do
       File.stub!(:exist?).and_return(true)
       ScaffoldGenerator.stub!(:new).with(@args).and_return(@generator)
       @generator.should_receive(:invoke_all)
-      @scaffold_runner.new.run(@args)
+      @runner_scaffold.new.run(@args)
     end
 
     it "cannot invoke scaffold generator outsite root folder" do
       File.stub!(:exist?).and_return(false)
       ScaffoldGenerator.should_not_receive(:new)
-      @scaffold_runner.new.run(@args)
+      @runner_scaffold.new.run(@args)
     end
   end
 end
