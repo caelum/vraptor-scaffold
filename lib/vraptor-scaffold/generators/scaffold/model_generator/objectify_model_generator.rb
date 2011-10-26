a9bac26b270a006941a6e4d512b9dcaecd54361c
Arthur Moura Carvalho <armoucar@gmail.com>
2011-10-26 02:20:05 -0200

    map a model in objectify factory resource

--- lib/vraptor-scaffold/generators/scaffold/model_generator/objectify_model_generator.rb
+++ lib/vraptor-scaffold/generators/scaffold/model_generator/objectify_model_generator.rb
@@ -7,5 +7,12 @@ class ObjectifyModelGenerator < BaseScaffold
   def build
     template("objectify_model.erb", Configuration.main_class_path(Configuration.models_package, "#{class_name}.java"))
     template("model_test.erb", Configuration.test_class_path(Configuration.models_package, "#{test_class_name}.java"))
+    map_orm_class
+  end
+
+  private
+  def map_orm_class
+    file = File.join Configuration::main_class_path, "infra", "ObjectifyFactory.java"
+    inject_into_file(file, "\t\tObjectifyService.register(#{class_name}.class);\n", :after => "\tstatic {\n")
   end
 end
