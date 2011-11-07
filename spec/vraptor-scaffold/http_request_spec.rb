318fbd171ac8cfc5e6cfbb1dbe1454751dcdde8e
Arthur Moura Carvalho <armoucar@gmail.com>
2011-11-07 14:39:49 -0200

    adjusting http request spec

--- spec/vraptor-scaffold/http_request_spec.rb
+++ spec/vraptor-scaffold/http_request_spec.rb
@@ -1,14 +1,15 @@
 require "spec_helper"
-require 'net/http'
 
 describe VraptorScaffold::HttpRequest do
   
   before :each do
+    @http_proxy = ENV['http_proxy']
     ENV['http_proxy'] = "http://google.com"
   end
 
   after :each do
     ENV.delete 'http_proxy'
+    ENV['http_proxy'] = @http_proxy
   end
 
   it "should use 'http_proxy' env when it is present" do
@@ -20,6 +21,10 @@ describe VraptorScaffold::HttpRequest do
 
     before :each do
       ENV.delete 'http_proxy'
+    end
+
+    after :each do
+      ENV['http_proxy'] = @http_proxy
     end
 
     it "should not use proxy settings when no http_proxy env exists" do
