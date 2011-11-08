394bd8d7ee6911d4325aad76c427fa56f14804a1
Rodolfo Liviero <rodolfoliviero@gmail.com>
2011-11-07 22:56:43 -0200

    cleaning http_request_spec

--- spec/vraptor-scaffold/http_request_spec.rb
+++ spec/vraptor-scaffold/http_request_spec.rb
@@ -1,30 +1,34 @@
 require "spec_helper"
 
 describe VraptorScaffold::HttpRequest do
-  
-  before :each do
+
+  before :all do
     @http_proxy = ENV['http_proxy']
-    ENV['http_proxy'] = "http://google.com"
   end
 
-  after :each do
-    ENV.delete 'http_proxy'
+  after :all do
     ENV['http_proxy'] = @http_proxy
   end
 
-  it "should use 'http_proxy' env when it is present" do
-    http = VraptorScaffold::HttpRequest.http
-    http.should be_true http.proxy_class?
+  context "http proxy" do
+    before :each do
+      ENV['http_proxy'] = "http://google.com"
+    end
+
+    after :each do
+      ENV.delete 'http_proxy'
+    end
+
+    it "should use 'http_proxy' env when it is present" do
+      http = VraptorScaffold::HttpRequest.http
+      http.should be_true http.proxy_class?
+    end
   end
 
   context "no proxy" do
 
     before :each do
       ENV.delete 'http_proxy'
-    end
-
-    after :each do
-      ENV['http_proxy'] = @http_proxy
     end
 
     it "should not use proxy settings when no http_proxy env exists" do
