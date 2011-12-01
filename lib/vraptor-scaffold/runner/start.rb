c03d88ddece0ff665c65873fea128e84ec590b37
Arthur Moura Carvalho <armoucar@gmail.com>
2011-12-01 13:33:34 -0200

    should not start a app outside root directory

--- lib/vraptor-scaffold/runner/start.rb
+++ lib/vraptor-scaffold/runner/start.rb
@@ -3,6 +3,10 @@ module VraptorScaffold
 
     class Start
       def run(args)
+        if File.exist?(:src)
+        else
+          Kernel.puts "To run vraptor start please go to the project root folder."
+        end
       end
     end
   end
--- spec/vraptor-scaffold/runner/start_spec.rb
+++ spec/vraptor-scaffold/runner/start_spec.rb
@@ -2,4 +2,14 @@ require "spec_helper"
 
 describe VraptorScaffold::Runner::Start do
 
+  before(:all) do
+    @start = VraptorScaffold::Runner::Start
+  end
+
+  it "cannot run start option outside root folder" do
+    File.stub!(:exist?).and_return(false)
+    Kernel.should_receive(:puts).with("To run vraptor start please go to the project root folder.")
+    @start.new.run nil
+  end
+
 end
