ccfc71a9a9e44ada8bbbdccc05b3e29a411f8609
Arthur Moura Carvalho <armoucar@gmail.com>
2011-12-01 17:29:36 -0200

    cannot run 'vraptor start' without a APPENGINE_SDK_HOME

--- lib/vraptor-scaffold/runner/start.rb
+++ lib/vraptor-scaffold/runner/start.rb
@@ -4,17 +4,22 @@ module VraptorScaffold
     class Start
       def run(args)
         validate
+        %x[sh $APPENGINE_SDK_HOME/bin/dev_appserver.sh #{Configuration::WEB_APP}]
       end
 
       private
 
       def validate
-        unless File.exist?(:src)
+        unless File.exist?("src")
           Kernel.puts "To run vraptor start please go to the project root folder." 
           Kernel::exit
         end
         unless Configuration.orm == "objectify"
-          Kernel.puts "vraptor start command is available only for gae apps"
+          Kernel.puts "vraptor start command is available only for gae apps."
+          Kernel::exit
+        end
+        unless ENV['APPENGINE_SDK_HOME']
+          Kernel.puts "To run vraptor start, configure the env var APPENGINE_SDK_HOME."
           Kernel::exit
         end
       end
--- spec/vraptor-scaffold/runner/start_spec.rb
+++ spec/vraptor-scaffold/runner/start_spec.rb
@@ -18,9 +18,27 @@ describe VraptorScaffold::Runner::Start do
     it "cannot start a app if objectify orm isn't used (gae app)" do
       File.stub!(:exist?).and_return(true)
       Configuration.stub!(:orm).and_return("hibernate")
-      Kernel.should_receive(:puts).with("vraptor start command is available only for gae apps")
+      Kernel.should_receive(:puts).with("vraptor start command is available only for gae apps.")
       lambda { @start.new.run nil }.should raise_error(SystemExit)
     end
 
+    context "without a APPENGINE_SDK_HOME" do
+
+      before(:all) do
+        @appengine_sdk_home = ENV['APPENGINE_SDK_HOME']
+        ENV.delete 'APPENGINE_SDK_HOME'
+      end
+
+      after :all do
+        ENV['APPENGINE_SDK_HOME'] = @appengine_sdk_home
+      end
+
+      it "should not run vraptor start" do
+        File.stub!(:exist?).and_return(true)
+        Configuration.stub!(:orm).and_return("objectify")
+        Kernel.should_receive(:puts).with("To run vraptor start, configure the env var APPENGINE_SDK_HOME.")
+        lambda { @start.new.run nil }.should raise_error(SystemExit)
+      end
+    end
   end
 end
