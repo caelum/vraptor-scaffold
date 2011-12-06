eab691eb4a85364ed2ebd97abf0ab6a56f3dec6b
Arthur Moura Carvalho <armoucar@gmail.com>
2011-12-06 00:31:52 -0200

    100% covered. refactored tests and created a new one.

--- lib/vraptor-scaffold/runner/start.rb
+++ lib/vraptor-scaffold/runner/start.rb
@@ -4,17 +4,15 @@ module VraptorScaffold
     class Start
       def run(args)
         validate
-        #%x[ant compile]
-        #%x[sh $APPENGINE_SDK_HOME/bin/dev_appserver.sh #{Configuration::WEB_APP}]
-        system("ant compile")
-        system("sh $APPENGINE_SDK_HOME/bin/dev_appserver.sh #{Configuration::WEB_APP}")
+        Kernel.system("ant compile")
+        Kernel.system("sh $APPENGINE_SDK_HOME/bin/dev_appserver.sh #{Configuration::WEB_APP}")
       end
 
       private
 
       def validate
         unless File.exist?("src")
-          Kernel.puts "To run vraptor start please go to the project root folder." 
+          Kernel.puts "To run vraptor start please go to the project root folder."
           Kernel::exit
         end
         unless Configuration.orm == "objectify"
--- spec/vraptor-scaffold/runner/start_spec.rb
+++ spec/vraptor-scaffold/runner/start_spec.rb
@@ -6,14 +6,21 @@ describe VraptorScaffold::Runner::Start do
     @start = VraptorScaffold::Runner::Start
   end
 
-  it "cannot run start option outside root folder" do
-    File.stub!(:exist?).and_return(false)
-    Kernel.should_receive(:puts).with("To run vraptor start please go to the project root folder.")
-    lambda { @start.new.run nil }.should raise_error(SystemExit)
-    
+  it "should execute shell command lines normally" do
+    File.stub!(:exist?).and_return(true)
+    Configuration.stub!(:orm).and_return("objectify")
+    ENV['APPENGINE_SDK_HOME'] = "any_local" unless ENV['APPENGINE_SDK_HOME']
+    Kernel.should_receive(:system).twice
+    @start.new.run nil
   end
 
-  context "starting a gae app" do
+  context "validate the startup of a gae app" do
+
+    it "cannot run start option outside root folder" do
+      File.stub!(:exist?).and_return(false)
+      Kernel.should_receive(:puts).with("To run vraptor start please go to the project root folder.")
+      lambda { @start.new.run nil }.should raise_error(SystemExit)
+    end
 
     it "cannot start a app if objectify orm isn't used (gae app)" do
       File.stub!(:exist?).and_return(true)
