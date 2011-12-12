3fdd93c76634298d89753c2246f0d64590a26db8
Rodolfo Liviero <rodolfoliviero@gmail.com>
2011-12-11 22:20:20 -0200

    little spec clean up

--- spec/vraptor-scaffold/runner/start_spec.rb
+++ spec/vraptor-scaffold/runner/start_spec.rb
@@ -6,7 +6,7 @@ describe VraptorScaffold::Runner::Start do
     @start = VraptorScaffold::Runner::Start
   end
 
-  it "should execute shell command lines normally" do
+  it "should call 'start' runner" do
     File.stub!(:exist?).and_return(true)
     Configuration.stub!(:orm).and_return("objectify")
     ENV['APPENGINE_SDK_HOME'] = "any_local" unless ENV['APPENGINE_SDK_HOME']
@@ -14,22 +14,22 @@ describe VraptorScaffold::Runner::Start do
     @start.new.run nil
   end
 
-  context "validate the startup of a gae app" do
+  context "validate" do
 
-    it "cannot run start option outside root folder" do
+    it "cannot run start outside root folder" do
       File.stub!(:exist?).and_return(false)
       Kernel.should_receive(:puts).with("To run vraptor start please go to the project root folder.")
       lambda { @start.new.run nil }.should raise_error(SystemExit)
     end
 
-    it "cannot start a app if objectify orm isn't used (gae app)" do
+    it "should start only gae apps" do
       File.stub!(:exist?).and_return(true)
       Configuration.stub!(:orm).and_return("hibernate")
       Kernel.should_receive(:puts).with("vraptor start command is available only for gae apps.")
       lambda { @start.new.run nil }.should raise_error(SystemExit)
     end
 
-    context "without a APPENGINE_SDK_HOME" do
+    context "without an APPENGINE_SDK_HOME" do
 
       before(:all) do
         @appengine_sdk_home = ENV['APPENGINE_SDK_HOME']
