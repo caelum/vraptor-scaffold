134f0b056ff24269f3975f860b24071beda522ec
Arthur Moura Carvalho <armoucar@gmail.com>
2012-06-27 16:58:45 -0400

    import manager

--- spec/vraptor-scaffold/generators/scaffold/import_manager_spec.rb
+++ spec/vraptor-scaffold/generators/scaffold/import_manager_spec.rb
@@ -1,11 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + "/../../../spec_helper")
 
 describe ImportManager do
-  
+
   it "should import package when type is BigDecimal" do
     ImportManager.new("big_decimal").package.include? "java.math.BigDecimal"
   end
-  
+
   it "should import package when type is BigInteger" do
     ImportManager.new("big_integer").package.include?("java.math.BigInteger").should be_true
   end
@@ -28,6 +28,6 @@ describe ImportManager do
 
   it "should import package when relationship is many to one" do
     ImportManager.new("references").package.include?("javax.persistence.ManyToOne").should be_true
-  end    
-  
+  end
+
 end
