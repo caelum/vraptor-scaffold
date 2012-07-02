d086cd09c933d5f2fb968c0fe8d74d34529b86c0
Arthur Moura Carvalho <armoucar@gmail.com>
2012-07-02 12:13:55 -0400

    changed project name created in objectify model generator. this ensures tests passing. Esquisito.

--- spec/vraptor-scaffold/generators/scaffold/model_generator/objectify_model_generator_spec.rb
+++ spec/vraptor-scaffold/generators/scaffold/model_generator/objectify_model_generator_spec.rb
@@ -4,20 +4,20 @@ describe ObjectifyModelGenerator do
 
   context "objectify" do
     before(:all) do
-      AppGenerator.new("src", ["-o=objectify", "--gae"]).invoke_all
-      FileUtils.chdir("src")
+      AppGenerator.new("gae-project", ["-o=objectify", "--gae"]).invoke_all
+      FileUtils.chdir("gae-project")
       @generator = ObjectifyModelGenerator.new("product", build_attributes)
       @generator.build
       FileUtils.chdir("..")
-      @project_path = "src"
+      @project_path = "gae-project"
     end
 
-    before(:each) do 
+    before(:each) do
       mock_objectify_config_file
     end
 
     after(:all) do
-      FileUtils.remove_dir "src"
+      FileUtils.remove_dir "gae-project"
     end
 
     it "should create model" do
@@ -33,7 +33,7 @@ describe ObjectifyModelGenerator do
 
     it "should map model in objetify factory" do
       source = File.join File.dirname(__FILE__), "templates", "ObjectifyFactory.java"
-      destination = File.join "src", Configuration::MAIN_SRC, "app", "infra", "ObjectifyFactory.java"
+      destination = File.join "gae-project", Configuration::MAIN_SRC, "app", "infra", "ObjectifyFactory.java"
       exists_and_identical?(source, destination)
     end
   end
