6bdaab77068a93ebb9b9f2d2f9e84b12cc5be2ef
Arthur Moura Carvalho <armoucar@gmail.com>
2012-07-17 19:59:13 -0300

    refactor dependency manager tests

--- spec/vraptor-scaffold/generators/app/dependency/dependency_manager_spec.rb
+++ spec/vraptor-scaffold/generators/app/dependency/dependency_manager_spec.rb
@@ -2,33 +2,20 @@ require File.expand_path(File.dirname(__FILE__) + "/../../../../spec_helper")
 
 describe DependencyManager do
 
-  before(:each) do
-    others_dependency = mock(Dependency)
-    others_dependency.stub!(:exclusions).and_return([])
-    Dependency.stub!(:new).and_return(others_dependency)
-    @dependency = mock(Dependency)
-  end
+	context "dependencies by project type" do
+		it "should be default" do
+			options = {}
+			DependencyManager.new(options).dependencies.class.should == DefaultDependencies
+		end
 
-  context "compile scope" do
-    it "should include freemarker dependency when template is freemarker" do
-      options = {:template_engine => "ftl"}
-      Dependency.stub!(:new).with("org.freemarker", "freemarker", "2.3.18").and_return(@dependency)
-      DependencyManager.new(options).compile_scope.include?(@dependency).should be_true
-    end
+		it "should be heroku" do
+			options = {:heroku => true}
+			DependencyManager.new(options).dependencies.class.should == HerokuDependencies
+		end
 
-    context "dependencies for a gae app" do
-      it "should include gae dependencies when gae project option is selected" do
-        options = {:gae => true}
-        Dependency.stub!(:new).with("com.googlecode.objectify", "objectify", "3.1").and_return(@dependency)
-        DependencyManager.new(options).compile_scope.include?(@dependency).should be_true
-      end
-
-      it "should not have hibernate persistence dependencies when gae project option is selected" do
-        options = {:gae => true}
-        Dependency.stub!(:new).with("org.hibernate", "hibernate-entitymanager", "3.6.7.Final").and_return(@dependency)
-        DependencyManager.new(options).compile_scope.include?(@dependency).should be_false
-      end
-    end
-  end
-
+		it "should be gae" do
+			options = {:gae => true}
+			DependencyManager.new(options).dependencies.class.should == GaeDependencies
+		end
+	end
 end
