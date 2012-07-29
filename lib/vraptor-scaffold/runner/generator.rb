a64bd7c281f13bc69651a20f01bec3b19ec5a19e
Arthur Moura Carvalho <armoucar@gmail.com>
2012-07-29 12:35:03 -0300

    resolving which app generator runs

--- lib/vraptor-scaffold/runner/generator.rb
+++ lib/vraptor-scaffold/runner/generator.rb
@@ -1,14 +1,24 @@
 module VraptorScaffold
   module Runner
 
+
     class Generator
       def run(args)
         project_path = args.shift
         if VraptorScaffold::Runner::Help.help? project_path
           AppGenerator.start(["-h"])
         else
-          AppGenerator.new(project_path, args).invoke_all
+          app_generators_available = {"default" => AppGenerator, "gae" => GaeAppGenerator }
+
+          app_generator_selected = "default"
+          app_generator_selected = "gae" if gae_project? args
+          app_generators_available.fetch(app_generator_selected).new(project_path, args).invoke_all
         end
+      end
+
+      private
+      def gae_project?(args)
+        !args.grep(/--gae/).empty?
       end
     end
   end
