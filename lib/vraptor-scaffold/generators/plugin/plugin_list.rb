6fd8da3ae3fb6ee893802511073aae0846d198b2
Arthur Moura Carvalho <armoucar@gmail.com>
2012-08-07 14:02:58 -0400

    tests yaml

--- lib/vraptor-scaffold/generators/plugin/plugin_list.rb
+++ lib/vraptor-scaffold/generators/plugin/plugin_list.rb
@@ -8,12 +8,27 @@ class PluginList
   def self.show
     Kernel.system("curl --progress-bar #{VRAPTOR_PLUGINS_URI} >> .vraptor-contrib-modules")
 
-    FileUtils.touch([".vraptor-contrib"])
-    vraptor_contrib = File.open(".vraptor-contrib", "w")
-    plugin_name_to_url = ""
+    create_new_plugins_file
 
+    vraptor_contrib = File.open(".vraptor-contrib", "w")
     modules = File.readlines(".vraptor-contrib-modules")
-    modules.each do |line|
+
+    plugin_name_to_url = organize_file(modules)
+
+    vraptor_contrib.puts(plugin_name_to_url)
+    FileUtils.rm(".vraptor-contrib-modules") if File.exists?(".vraptor-contrib-modules")
+    YAML.load_file(".vraptor-contrib")
+  end
+
+  private
+  def self.create_new_plugins_file
+    FileUtils.rm(".vraptor-contrib") if File.exists?(".vraptor-contrib")
+    FileUtils.touch(".vraptor-contrib")
+  end
+
+  def self.organize_file(git_modules_file)
+    plugin_name_to_url = ""
+    git_modules_file.each do |line|
       if line[/path =/]
         plugin_name = line[/= (.*)/][2..-1]
         plugin_name_to_url << plugin_name
@@ -24,11 +39,9 @@ class PluginList
         plugin_name_to_url << " = "
         plugin_name_to_url << plugin_address
         plugin_name_to_url << "\n"
-
       end
     end
 
-    vraptor_contrib.puts(plugin_name_to_url)
+    plugin_name_to_url
   end
-
 end
